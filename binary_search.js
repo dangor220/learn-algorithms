d0a23f28f0b0d9f9b6bff80ba601a85de2b351f3
Danil Gordeev <dan.gor220@yandex.ru>
2022-08-30 22:00:52 +0300

    completed: binary search task

--- binary_search.js
+++ binary_search.js
@@ -1,2 +1,26 @@
-'use strict'
+'use strict';
 
+// Функция бинарного поиска по отсортированному массиву с поиском числа n.
+
+const arr = [1, 3, 22, 31, 55];
+
+const binarySearch = (arr, n) => {
+	let low = 0,
+		  hight = arr.length - 1;
+
+	while (low <= hight) {
+		let middle = Math.floor((low + hight) / 2);
+		let guess = arr[middle];
+
+		if (guess === n) {
+			return middle;
+		} else if (guess > n) {
+			hight = middle - 1;
+		} else {
+			low = middle + 1;
+		}
+	}
+	return 'Значение не найдено';
+};
+
+console.log(binarySearch(arr, 1));
